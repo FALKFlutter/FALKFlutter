401ad3bd6c8dd912d229d270ab491efeb4ede4f7
kailashbharat <kailashbb12@hotmail.com>
2021-07-11 08:18:14 +0200

    added a server and mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
-/**
- * Server file
- */
+const express = require("express")
+const mongoose = require("mongoose")
+const{MONGOURL} = require("./keys")
 
-var bestSexPos = 69;
-let a = 5;
+const app = express()
+const PORT = 3000
+
+mongoose.connect(MONGOURL, {useNewUrlParser: true, useUnifiedTopology: true})
+
+mongoose.connection.once("open",()=>{
+    console.log("succesfully connected to db")
+})
+
+mongoose.connection.on("error", console.error.bind(console, "connection error:"))
+
+
+app.get("/",(req,res)=>{
+    res.send("hi")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`)
+})
