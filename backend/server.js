45837333c318a878229cd91eb0b92aa97c4eb95d
kailashbharat <kailashbb12@hotmail.com>
2021-07-12 15:38:55 +0200

    added register and login routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 const express = require("express")
 const mongoose = require("mongoose");
+// let MONGOURL = MONGOURL === process.env.MONGOURL ? process.env.MONGOURL :require('./keys').MONGOURL 
+
 let MONGOURL;
 if(process.env.MONGOURL){
     MONGOURL = process.env.MONGOURL
 }else{
     MONGOURL = require('./keys').MONGOURL;
 }
-/**
- * || = 0 "" null undefined
- * ?? = null undefined
- * 
- */
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
+
+require("./models/user")
+mongoose.model("User")
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(require("./routes/auth"))
 
 mongoose.connect(MONGOURL, {useNewUrlParser: true, useUnifiedTopology: true})
 
@@ -23,10 +27,6 @@ mongoose.connection.once("open",()=>{
 
 mongoose.connection.on("error", console.error.bind(console, "connection error:"))
 
-
-app.get("/",(req,res)=>{
-    res.send("hi")
-})
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
