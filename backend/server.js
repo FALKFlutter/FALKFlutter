b4f2bd9f02c0a02b2da987bbec82cd2970e3fdc3
kailashbharat <kailashbb12@hotmail.com>
2021-07-15 07:58:09 +0200

    adjusted the requested changes from jay

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,31 @@
-const express = require("express")
+const express = require("express");
 const mongoose = require("mongoose");
 
-const MONGOURL = process.env.MONGOURL ? process.env.MONGOURL :require('./keys').MONGOURL 
+const MONGOURL = process.env.MONGOURL
+  ? process.env.MONGOURL
+  : require("./keys").MONGOURL;
 
-const app = express()
-const PORT = process.env.PORT || 4000
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-require("./models/user")
-mongoose.model("User")
+require("./models/user");
+mongoose.model("User");
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(require("./routes/auth"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require("./routes/auth"));
 
-mongoose.connect(MONGOURL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connection.once("open",()=>{
-    console.log("succesfully connected to db")
-})
+mongoose.connection.once("open", () => {
+  console.log("succesfully connected to db");
+});
 
-mongoose.connection.on("error", console.error.bind(console, "connection error:"))
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "connection error:")
+);
 
-
-app.listen(PORT, ()=>{
-    console.log(`Listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
